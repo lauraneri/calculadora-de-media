7206c295379e567663ce98ddce8f9daa08d9236e
Laura Neri <laura6neri@yahoo.com.br>
2021-06-04 12:57:32 -0300

    Foi inserido o nome das desenvolvedoras e um pequeno comentário

--- app/src/main/java/com/example/calculadorademedia/MainActivity.java
+++ app/src/main/java/com/example/calculadorademedia/MainActivity.java
@@ -1,3 +1,9 @@
+//Nomes:
+// Laura Neri Thomaz da Silva - SC1901311
+// Luiza Maria da Silva - SC1901354
+// Professor, nós seguimos igualzinho na aula, porém nenhuma de nós duas conseguimos rodar
+// acreditamos que esteja tudo certo, mas só deixando avisado caso tenha acontecido algo inesperado
+
 package com.example.calculadorademedia;
 
 import androidx.appcompat.app.AppCompatActivity;
